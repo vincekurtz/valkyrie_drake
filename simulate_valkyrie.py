f4491a16a93070974a5a671ecd59188bda54b632
Vince Kurtz <vjkurtz@gmail.com>
2019-07-31 11:38:47 -0400

    complete change to MultibodyPlant framework

--- simulate_valkyrie.py
+++ simulate_valkyrie.py
@@ -5,7 +5,7 @@ from helpers import RPYValkyrieFixedPointState, RPYValkyrieFixedPointTorque
 from controllers import ValkyrieController
 import numpy as np
 
-# Load the model from a urdf file
+# Load the valkyrie model from a urdf file
 robot_description_file = "drake/examples/valkyrie/urdf/urdf/valkyrie_A_sim_drake_one_neck_dof_wide_ankle_rom.urdf"
 robot_urdf = FindResourceOrThrow(robot_description_file)
 builder = DiagramBuilder()
@@ -13,12 +13,27 @@ scene_graph = builder.AddSystem(SceneGraph())
 robot = builder.AddSystem(MultibodyPlant(time_step=0))   # 0 for continuous, dt for discrete
 robot.RegisterAsSourceForSceneGraph(scene_graph)
 Parser(plant=robot).AddModelFromFile(robot_urdf)
+
+# Add a flat ground with friction
+X_BG = RigidTransform()
+surface_friction = CoulombFriction(
+        static_friction = 0.7,
+        dynamic_friction = 0.1)
+robot.RegisterCollisionGeometry(
+        robot.world_body(),      # the body for which this object is registered
+        X_BG,                    # The fixed pose of the geometry frame G in the body frame B
+        HalfSpace(),             # Defines the geometry of the object
+        "ground_collision",      # A name
+        surface_friction)        # Coulomb friction coefficients
+robot.RegisterVisualGeometry(
+        robot.world_body(),
+        X_BG,
+        HalfSpace(),
+        "ground_visual",
+        np.array([0.5,0.5,0.5,0.7]))    # Color
+
 robot.Finalize()
 assert robot.geometry_source_is_registered()
-
-#tree = RigidBodyTree(robot_urdf, FloatingBaseType.kRollPitchYaw)
-
-#AddFlatTerrainToWorld(tree, 100, 10)  # add some flat terrain to walk on
 
 # Set up the Scene Graph
 builder.Connect(
@@ -39,7 +54,7 @@ robot_context = diagram.GetMutableSubsystemContext(robot, diagram_context)
 zero_cmd = np.zeros(robot.num_actuators())
 robot_context.FixInputPort(
         robot.get_actuation_input_port().get_index(), 
-        np.zeros(30))
+        np.zeros(30)+0.5)
 
 
 # Simulator setup
@@ -48,14 +63,16 @@ simulator.set_target_realtime_rate(1.0)
 simulator.set_publish_every_time_step(False)
 
 # Set initial state
-#state = context.get_mutable_continuous_state_vector()
+state = robot_context.get_mutable_continuous_state_vector()
+initial_state_vec = np.zeros(robot.num_positions()+robot.num_velocities())
+initial_state_vec[0] = 1
+initial_state_vec[6] = 1.2
 #initial_state_vec = RPYValkyrieFixedPointState()  # computes [q,qd] for a reasonable starting position
-#state.SetFromVector(initial_state_vec)
+state.SetFromVector(initial_state_vec)
 
 # Use a different integrator to speed up simulation (default is RK3)
-#context = simulator.get_mutable_context()
-#integrator = RungeKutta2Integrator(diagram, 1e-3, context)
-#simulator.reset_integrator(integrator)
+integrator = RungeKutta2Integrator(diagram, 5e-4, diagram_context)
+simulator.reset_integrator(integrator)
 
 # Run the simulation
 simulator.Initialize()
