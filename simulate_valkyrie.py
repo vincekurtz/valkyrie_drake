66bf691acc3b3ec7f45219c5d9097df3581fdd53
Vince Kurtz <vjkurtz@gmail.com>
2019-07-31 10:34:00 -0400

    start conversion from depreciated RigidBodyTree to MultibodyPlant

--- simulate_valkyrie.py
+++ simulate_valkyrie.py
@@ -8,66 +8,54 @@ import numpy as np
 # Load the model from a urdf file
 robot_description_file = "drake/examples/valkyrie/urdf/urdf/valkyrie_A_sim_drake_one_neck_dof_wide_ankle_rom.urdf"
 robot_urdf = FindResourceOrThrow(robot_description_file)
-tree = RigidBodyTree(robot_urdf, FloatingBaseType.kRollPitchYaw)
-
-AddFlatTerrainToWorld(tree, 100, 10)  # add some flat terrain to walk on
-
-# See some model parameters
-print(tree.get_num_positions())
-print(tree.get_num_velocities())
-print(tree.get_num_actuators())
-print(tree.get_num_bodies())
-print("")
-
-# Get equations of motion for given q, qd
-q = np.zeros(tree.get_num_positions())
-qd = np.zeros(tree.get_num_velocities())
-cache = tree.doKinematics(q,qd)
-
-bodies = [tree.get_body(j) for j in range(tree.get_num_bodies())]
-no_wrench = { body : np.zeros(6) for body in bodies}
-
-C = tree.dynamicsBiasTerm(cache, no_wrench)  # correolis and centripedal terms
-H = tree.massMatrix(cache)                   # mass matrix
-
-print(C.shape)
-print(H.shape)
-
-# Centroidal momentum quantities
-A = tree.centroidalMomentumMatrix(cache)
-Ad_qd = tree.centroidalMomentumMatrixDotTimesV(cache)
-print(A.shape)
-print(Ad_qd.shape)
-
-# Simulation setup
 builder = DiagramBuilder()
-lc = DrakeLcm()
-robot = builder.AddSystem(RigidBodyPlant(tree))
-visualizer_publisher = builder.AddSystem(DrakeVisualizer(tree,lc))
-builder.Connect(robot.get_output_port(0), visualizer_publisher.get_input_port(0))
+scene_graph = builder.AddSystem(SceneGraph())
+robot = builder.AddSystem(MultibodyPlant(time_step=0))   # 0 for continuous, dt for discrete
+robot.RegisterAsSourceForSceneGraph(scene_graph)
+Parser(plant=robot).AddModelFromFile(robot_urdf)
+robot.Finalize()
+assert robot.geometry_source_is_registered()
 
-# Apply constant torques to the joints
-#tau_vec = np.zeros(tree.get_num_actuators())
-#force = builder.AddSystem(ConstantVectorSource(tau_vec))
-#builder.Connect(force.get_output_port(0), robot.get_input_port(0))
-controller = builder.AddSystem(ValkyrieController())
-builder.Connect(controller.get_output_port(0), robot.get_input_port(0))
-builder.Connect(robot.get_output_port(0),controller.get_input_port(0))
+#tree = RigidBodyTree(robot_urdf, FloatingBaseType.kRollPitchYaw)
 
+#AddFlatTerrainToWorld(tree, 100, 10)  # add some flat terrain to walk on
+
+# Set up the Scene Graph
+builder.Connect(
+        scene_graph.get_query_output_port(),
+        robot.get_geometry_query_input_port())
+builder.Connect(
+        robot.get_geometry_poses_output_port(),
+        scene_graph.get_source_pose_port(robot.get_source_id()))
+
+# Connect the visualizer
+ConnectDrakeVisualizer(builder=builder, scene_graph=scene_graph)
 diagram = builder.Build()
-simulator = Simulator(diagram)
+
+diagram_context = diagram.CreateDefaultContext()
+robot_context = diagram.GetMutableSubsystemContext(robot, diagram_context)
+
+# Send fixed commands of zero to the joints
+zero_cmd = np.zeros(robot.num_actuators())
+robot_context.FixInputPort(
+        robot.get_actuation_input_port().get_index(), 
+        np.zeros(30))
+
+
+# Simulator setup
+simulator = Simulator(diagram, diagram_context)
 simulator.set_target_realtime_rate(1.0)
 simulator.set_publish_every_time_step(False)
-context = simulator.get_mutable_context()
 
 # Set initial state
-state = context.get_mutable_continuous_state_vector()
-initial_state_vec = RPYValkyrieFixedPointState()  # computes [q,qd] for a reasonable starting position
-state.SetFromVector(initial_state_vec)
+#state = context.get_mutable_continuous_state_vector()
+#initial_state_vec = RPYValkyrieFixedPointState()  # computes [q,qd] for a reasonable starting position
+#state.SetFromVector(initial_state_vec)
 
 # Use a different integrator to speed up simulation (default is RK3)
-integrator = RungeKutta2Integrator(diagram, 1e-3, context)
-simulator.reset_integrator(integrator)
+#context = simulator.get_mutable_context()
+#integrator = RungeKutta2Integrator(diagram, 1e-3, context)
+#simulator.reset_integrator(integrator)
 
 # Run the simulation
 simulator.Initialize()
