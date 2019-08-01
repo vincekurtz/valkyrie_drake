7141249cfc8e864593b47fb6d53fb606c7e3a38a
Vince Kurtz <vjkurtz@gmail.com>
2019-08-01 13:45:41 -0400

    simple joint-space PD approach to (almost) balance the robot

--- controllers.py
+++ controllers.py
@@ -5,6 +5,7 @@
 ##
 
 import numpy as np
+import time
 from pydrake.all import *
 from helpers import *
 
@@ -21,35 +22,10 @@ class ValkyrieController(VectorSystem):
         
         self.robot = robot
 
-        # PD parameters copied from drake/examples/valkyrie/valkyrie_pd_ff_controller.cc
-        Kp_vec = np.asarray([0, 0, 0, 0, 0, 0])                       # base
-        Kp_vec = np.hstack((Kp_vec,[100, 300, 300]))                 # spine
-	Kp_vec = np.hstack((Kp_vec,[10]))                            # neck
-	Kp_vec = np.hstack((Kp_vec,[10, 10, 10]))                    # r shoulder
-	Kp_vec = np.hstack((Kp_vec,[1, 1, 0.1, 0.1]))                # r arm
-	Kp_vec = np.hstack((Kp_vec,[10, 10, 10]))                    # l shoulder
-	Kp_vec = np.hstack((Kp_vec,[1, 1, 0.1, 0.1]))                # l arm
-	Kp_vec = np.hstack((Kp_vec,[100, 100, 300, 300, 300, 100]))  # r leg
-	Kp_vec = np.hstack((Kp_vec,[100, 100, 300, 300, 300, 100]))  # l leg
-
-        self.Kp = np.diag(Kp_vec)
-
-        Kd_vec = np.asarray([0, 0, 0, 0, 0, 0])                       # base
-        Kd_vec = np.hstack((Kd_vec,[10, 10, 10]))                    # spine
-	Kd_vec = np.hstack((Kd_vec,[3]))                             # neck
-	Kd_vec = np.hstack((Kd_vec,[3, 3, 3]))                       # r shoulder
-	Kd_vec = np.hstack((Kd_vec,[0.1, 0.1, 0.01, 0.01]))          # r arm
-	Kd_vec = np.hstack((Kd_vec,[3, 3, 3]))                       # l shoulder
-	Kd_vec = np.hstack((Kd_vec,[0.1, 0.1, 0.01, 0.01]))          # l arm
-	Kd_vec = np.hstack((Kd_vec,[10, 10, 10, 10, 10, 10]))        # r leg
-	Kd_vec = np.hstack((Kd_vec,[10, 10, 10, 10, 10, 10]))        # l leg
-
-        self.Kd = np.diag(Kd_vec)
-
         # Nominal state
         self.nominal_state = ValkyrieFixedPointState()
 
-    def DoCalcVectorOutput(self, context, state, unused, output):
+    def DoCalcVectorOutput(self, leaf_context, state, unused, output):
         """
         Map from the state (q,qd) to output torques.
         """
@@ -57,8 +33,20 @@ class ValkyrieController(VectorSystem):
         q = state[0:self.robot.num_positions()]
         v = state[self.robot.num_positions():]
 
+        q_des = self.nominal_state[0:self.robot.num_positions()]
+        v_des = self.nominal_state[self.robot.num_positions()]
+
         M, Cv, tauG, B, tauExt = ManipulatorDynamics(self.robot, q, v)
 
-        print(B)
+        context = self.robot.CreateDefaultContext()
+        q_err = self.robot.MapQDotToVelocity(context, q_des-q)   # need to use this mapping since positions
+                                                                 # use quaternions
 
-        output[:] = 0
+        v_err = v_des - v
+
+        # simple PD scheme to determine vd_Des
+        vd_des = 100*v_err + 10000*q_err
+
+        u = solve_joint_accel_QP(M,Cv,tauG,B,tauExt,vd_des)
+
+        output[:] = u
--- simulate_valkyrie.py
+++ simulate_valkyrie.py
@@ -10,60 +10,60 @@ robot_description_file = "drake/examples/valkyrie/urdf/urdf/valkyrie_A_sim_drake
 robot_urdf = FindResourceOrThrow(robot_description_file)
 builder = DiagramBuilder()
 scene_graph = builder.AddSystem(SceneGraph())
-robot = builder.AddSystem(MultibodyPlant(time_step=1e-3))
-robot.RegisterAsSourceForSceneGraph(scene_graph)
-Parser(plant=robot).AddModelFromFile(robot_urdf)
+plant = builder.AddSystem(MultibodyPlant(time_step=1e-3))
+plant.RegisterAsSourceForSceneGraph(scene_graph)
+Parser(plant=plant).AddModelFromFile(robot_urdf)
 
 # Add a flat ground with friction
 X_BG = RigidTransform()
 surface_friction = CoulombFriction(
         static_friction = 0.7,
         dynamic_friction = 0.1)
-robot.RegisterCollisionGeometry(
-        robot.world_body(),      # the body for which this object is registered
+plant.RegisterCollisionGeometry(
+        plant.world_body(),      # the body for which this object is registered
         X_BG,                    # The fixed pose of the geometry frame G in the body frame B
         HalfSpace(),             # Defines the geometry of the object
         "ground_collision",      # A name
         surface_friction)        # Coulomb friction coefficients
-robot.RegisterVisualGeometry(
-        robot.world_body(),
+plant.RegisterVisualGeometry(
+        plant.world_body(),
         X_BG,
         HalfSpace(),
         "ground_visual",
         np.array([0.5,0.5,0.5,0.0]))    # Color set to be completely transparent
 
 # Turn off gravity (for testing)
-#g = robot.mutable_gravity_field()
+#g = plant.mutable_gravity_field()
 #g.set_gravity_vector([0,0,0])
 
-robot.Finalize()
-assert robot.geometry_source_is_registered()
+plant.Finalize()
+assert plant.geometry_source_is_registered()
 
 # Set up the Scene Graph
 builder.Connect(
         scene_graph.get_query_output_port(),
-        robot.get_geometry_query_input_port())
+        plant.get_geometry_query_input_port())
 builder.Connect(
-        robot.get_geometry_poses_output_port(),
-        scene_graph.get_source_pose_port(robot.get_source_id()))
+        plant.get_geometry_poses_output_port(),
+        scene_graph.get_source_pose_port(plant.get_source_id()))
 
 # Set up a controller
-controller = builder.AddSystem(ValkyrieController(robot))
+controller = builder.AddSystem(ValkyrieController(plant))
 builder.Connect(
-        robot.get_state_output_port(),
+        plant.get_state_output_port(),
         controller.get_input_port(0))
 builder.Connect(
         controller.get_output_port(0),
-        robot.get_actuation_input_port(robot.GetModelInstanceByName('valkyrie')))
+        plant.get_actuation_input_port(plant.GetModelInstanceByName('valkyrie')))
 
 # Set up the Visualizer
 ConnectDrakeVisualizer(builder=builder, scene_graph=scene_graph)
-ConnectContactResultsToDrakeVisualizer(builder, robot)
+ConnectContactResultsToDrakeVisualizer(builder, plant)
 
 # Compile the diagram: no adding control blocks from here on out
 diagram = builder.Build()
 diagram_context = diagram.CreateDefaultContext()
-robot_context = diagram.GetMutableSubsystemContext(robot, diagram_context)
+plant_context = diagram.GetMutableSubsystemContext(plant, diagram_context)
 
 # Simulator setup
 simulator = Simulator(diagram, diagram_context)
@@ -71,7 +71,7 @@ simulator.set_target_realtime_rate(1.0)
 simulator.set_publish_every_time_step(False)
 
 # Set initial state
-state = robot_context.get_mutable_discrete_state_vector()
+state = plant_context.get_mutable_discrete_state_vector()
 initial_state_vec = ValkyrieFixedPointState()  # computes [q,qd] for a reasonable starting position
 state.SetFromVector(initial_state_vec)
 
@@ -81,4 +81,4 @@ simulator.reset_integrator(integrator)
 
 # Run the simulation
 simulator.Initialize()
-simulator.AdvanceTo(1.0)
+simulator.AdvanceTo(0.50)
