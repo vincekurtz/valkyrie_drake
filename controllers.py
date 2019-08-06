0903d10edeb7c149906b52b214b146b1fb1563cd
Vince Kurtz <vjkurtz@gmail.com>
2019-08-06 14:53:39 -0400

    add contact acceleration constraint

--- controllers.py
+++ controllers.py
@@ -52,28 +52,28 @@ class ValkyrieController(VectorSystem):
         world_index = self.tree.world().get_body_index()
 
         contact_jacobians = []
+        contact_jacobians_dot_v = []
 
         # Right foot contacts
-        for contact_point in self.get_foot_contact_points():
-            contact_jacobian = self.tree.transformPointsJacobian(cache,             # kinematics cache
-                                                                 contact_point,     # point in foot frame
-                                                                 right_foot_index,  # foot frame index
-                                                                 world_index,       # world frame index
-                                                                 False)             # in terms of qd
-            contact_jacobians.append(contact_jacobian)
+        for foot_index in [right_foot_index, left_foot_index]:
+            for contact_point in self.get_foot_contact_points():
+                J = self.tree.transformPointsJacobian(cache,             # kinematics cache
+                                                      contact_point,     # point in foot frame
+                                                      foot_index,        # foot frame index
+                                                      world_index,       # world frame index
+                                                      False)             # in terms of qd
 
-        # Left foot contacts
-        for contact_point in self.get_foot_contact_points():
-            contact_jacobian = self.tree.transformPointsJacobian(cache,
-                                                                 contact_point,
-                                                                 left_foot_index,
-                                                                 world_index,
-                                                                 False)
-            contact_jacobians.append(contact_jacobian)
+                Jd_v = self.tree.transformPointsJacobianDotTimesV(cache,
+                                                                  contact_point,
+                                                                  foot_index,
+                                                                  world_index)
+                                                                      
+                contact_jacobians.append(J)
+                contact_jacobians_dot_v.append(Jd_v)
 
-        return contact_jacobians
+        return (contact_jacobians, contact_jacobians_dot_v)
 
-    def solve_QP(self, cache, contact_jacobians, xdd_com_des, qdd_des):
+    def solve_QP(self, cache, contact_jacobians, contact_jacobians_dot_v, xdd_com_des, qdd_des):
         """
         Solve a quadratic program which attempts to regulate the joints to the desired
         accelerations and center of mass to the desired position as follows:
@@ -81,12 +81,14 @@ class ValkyrieController(VectorSystem):
             min   w_1*| Jcom*qdd + Jcomd*qd - xdd_com_des |^2 + w_2*| qdd_des - qdd |^2
             s.t.  H*qdd + C = B*tau + sum(J'*f)
                   f \in friction cones
+                  J*qdd + Jd*qd == 0  for all contacts
         """
         mp = MathematicalProgram()
+        num_contacts = len(contact_jacobians)
 
         # Set weightings for prioritized task-space control
         w1 = 1.0   # center-of-mass tracking
-        w2 = 0.5   # joint tracking
+        w2 = 0.1   # joint tracking
 
         # Get dynamic quantities
         H = self.tree.massMatrix(cache)
@@ -94,40 +96,40 @@ class ValkyrieController(VectorSystem):
         B = self.tree.B
 
         Jcom = self.tree.centerOfMassJacobian(cache)
-        Jcomd_qd = self.tree.centerOfMassJacobianDotTimesV(cache)[np.newaxis].T
+        Jcomd_qd = self.tree.centerOfMassJacobianDotTimesV(cache)
 
         # create optimization variables
         qdd = mp.NewContinuousVariables(self.nv, 'qdd')   # joint accelerations
         tau = mp.NewContinuousVariables(self.nu, 'tau')   # applied torques
        
         f_contact = {}
-        for i in range(len(contact_jacobians)):        # contact forces
+        for i in range(num_contacts):        # contact forces
             f_contact[i] = mp.NewContinuousVariables(3, 'f_%s'%i)
 
-        # Center of mass tracking cost
-        xdd_com_des = xdd_com_des[np.newaxis].T    # cast as vectors for numpy
+        # Cast vectors as nx1 numpy arrays to allow for matrix multiplication with np.dot()
+        C = C[np.newaxis].T
+        Jcomd_qd = Jcomd_qd[np.newaxis].T
+        xdd_com_des = xdd_com_des[np.newaxis].T
+        tau = tau[np.newaxis].T
         qdd = qdd[np.newaxis].T
-        x_com_err = np.dot(Jcom,qdd) + Jcomd_qd - xdd_com_des
+        qdd_des = qdd_des[np.newaxis].T
 
+        # Center of mass tracking cost
+        x_com_err = np.dot(Jcom,qdd) + Jcomd_qd - xdd_com_des
         x_com_cost = np.dot(x_com_err.T,x_com_err)    # this is a 1x1 np.ndarray and we need a pydrake.symbolic.Expression
         x_com_cost = x_com_cost[0,0]                  # to use as a cost, so we simply extract the first element.
 
         mp.AddQuadraticCost(w1*x_com_cost)
        
         # Joint tracking cost
-        qdd_des = qdd_des[np.newaxis].T
         q_err = qdd - qdd_des
         q_cost = np.dot(q_err.T,q_err)
         q_cost = q_cost[0,0]
 
         mp.AddQuadraticCost(w2*q_cost)
         
-
         # Dynamic constraints 
-        C = C[np.newaxis].T         # cast as vectors for numpy multiplication
-        tau = tau[np.newaxis].T
-
-        f_ext = sum([np.dot(contact_jacobians[i].T, f_contact[i][np.newaxis].T) for i in range(len(contact_jacobians))])
+        f_ext = sum([np.dot(contact_jacobians[i].T, f_contact[i][np.newaxis].T) for i in range(num_contacts)])
         lhs = np.dot(H,qdd) + C
         rhs = np.dot(B,tau) + f_ext
 
@@ -135,11 +137,20 @@ class ValkyrieController(VectorSystem):
             mp.AddLinearConstraint(lhs[i,0] == rhs[i,0])
 
         # Friction cone (really pyramid) constraints 
-        for j in range(len(contact_jacobians)):
+        for j in range(num_contacts):
             mp.AddConstraint(f_contact[j][0] + f_contact[j][1] <= self.mu*f_contact[j][2])
             mp.AddConstraint(-f_contact[j][0] + f_contact[j][1] <= self.mu*f_contact[j][2])
             mp.AddConstraint(f_contact[j][0] - f_contact[j][1] <= self.mu*f_contact[j][2])
             mp.AddConstraint(-f_contact[j][0] - f_contact[j][1] <= self.mu*f_contact[j][2])
+
+        # Contact acceleration constraints
+        for j in range(num_contacts):
+            J = contact_jacobians[j]
+            Jd_qd = contact_jacobians_dot_v[j][np.newaxis].T
+
+            contact_accel = np.dot(J,qdd) + Jd_qd
+            for i in range(3):
+                mp.AddConstraint(contact_accel[i,0] == 0)
        
         # Solve the QP
         result = Solve(mp)
@@ -161,7 +172,7 @@ class ValkyrieController(VectorSystem):
         cache = self.tree.doKinematics(q,qd)
 
         # Compute contact jacobians
-        contact_jacobians = self.get_contact_jacobians(cache)
+        contact_jacobians, contact_jacobians_dot_v = self.get_contact_jacobians(cache)
 
         # Compute desired joint acclerations
         q_nom = self.nominal_state[:self.np]
@@ -180,7 +191,7 @@ class ValkyrieController(VectorSystem):
         xdd_com_des = 100*(x_com_nom-x_com) + 10*(xd_com_nom-xd_com)
 
         # Solve QP to get desired torques
-        tau_qp = self.solve_QP(cache, contact_jacobians, xdd_com_des, qdd_des)
+        tau_qp = self.solve_QP(cache, contact_jacobians, contact_jacobians_dot_v, xdd_com_des, qdd_des)
 
         output[:] = tau_qp
         
