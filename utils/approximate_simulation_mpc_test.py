cafe59a7a02edd1fe609f932701d2f7f06212a28
Vince Kurtz <vjkurtz@gmail.com>
2019-11-06 15:04:12 -0500

    generating reasonable interface via K-M optimization

--- utils/approximate_simulation_mpc_test.py
+++ utils/approximate_simulation_mpc_test.py
@@ -72,25 +72,31 @@ C_task = np.eye(9)
 
 interface_mp = MathematicalProgram()
 
-# Choose feedback control gain K via LQR
-Q_task = np.diag([2000,2000,10, 1,1,1, 1,1,1])
-R_task = 0.01*np.eye(6)
-K, P = LinearQuadraticRegulator(A_task, B_task, Q_task, R_task)
-K = -K
+# Find M, K by solving an SDP
+lmbda = 0.01
+Mbar = interface_mp.NewSymmetricContinuousVariables(9,"Mbar")
+Kbar = interface_mp.NewContinuousVariables(6,9,"Kbar")
 
-# Find M by solving an SDP
-lmbda = 0.001
-M = interface_mp.NewSymmetricContinuousVariables(9,"M")
+sdcon_1 = np.vstack([
+          np.hstack([ Mbar,                np.dot(Mbar,C_task.T) ]),
+          np.hstack([ np.dot(C_task,Mbar), np.eye(9)             ])])
 
-interface_mp.AddPositiveSemidefiniteConstraint(M - np.dot(C_task.T, C_task))
+sdcon_2 = -(np.dot(Mbar, A_task.T) + np.dot(A_task, Mbar) + np.dot(Kbar.T,B_task.T) + np.dot(B_task,Kbar) + 2*lmbda*Mbar)
 
-AplusBK = A_task + np.dot(B_task, K)
-interface_mp.AddPositiveSemidefiniteConstraint(-2*lmbda*M - np.dot(AplusBK.T,M) - np.dot(M,AplusBK) )
+# Add semidefinite constraints with some epsilon for numerical stability
+interface_mp.AddPositiveSemidefiniteConstraint(sdcon_1 - 1e-4*np.eye(sdcon_1.shape[0]))
+interface_mp.AddPositiveSemidefiniteConstraint(sdcon_2 - 1e-4*np.eye(sdcon_2.shape[0]))
+interface_mp.AddPositiveSemidefiniteConstraint(Mbar-1e-4*np.eye(9))
+
+#interface_mp.AddCost(np.trace(Mbar))
+interface_mp.AddCost(np.trace(np.dot(Kbar.T,Kbar)))  # penalize high control gains
 
 result = Solve(interface_mp)
 
 assert result.is_success(), "Interface SDP infeasible"
-M = result.GetSolution(M)
+print(result.GetSolution(Kbar))
+M = np.linalg.inv(result.GetSolution(Mbar))
+K = np.dot(result.GetSolution(Kbar),M)
 
 # Choose P, Q, and R by hand
 P = C_lip
@@ -114,6 +120,13 @@ assert np.all(C_lip == np.dot(C_task,P)) , "Failed test C_lip = C_task*P"
 
 assert np.all( np.dot(P,A_lip) == np.dot(A_task,P) + np.dot(B_task,Q) ) \
             , "Failed Test P*A_lip = A_task*P+B*Q"
+
+# Choose feedback control gain K via LQR
+#Q_task = np.diag([2000,2000,10, 1,1,1, 1,1,1])
+#R_task = 1.01*np.eye(6)
+#K, V = LinearQuadraticRegulator(A_task, B_task, Q_task, R_task)
+#K = -K
+
 
 ###############################################
 # MPC formulation
@@ -161,9 +174,9 @@ def perform_template_mpc(t, x_lip_init, x_task_init):
                                           dt)
 
         # Add interface constraint
-        A_interface = np.hstack([R, (Q-np.dot(K,P)), K, -np.eye(6)])
-        x_interface = np.hstack([u_lip[:,i],x_lip[:,i],x_task[:,i],u_task[:,i]])[np.newaxis].T
-        mp.AddLinearEqualityConstraint(A_interface, np.zeros((6,1)), x_interface)
+        #A_interface = np.hstack([R, (Q-np.dot(K,P)), K, -np.eye(6)])
+        #x_interface = np.hstack([u_lip[:,i],x_lip[:,i],x_task[:,i],u_task[:,i]])[np.newaxis].T
+        #mp.AddLinearEqualityConstraint(A_interface, np.zeros((6,1)), x_interface)
 
 
     # Add terminal cost
@@ -212,6 +225,10 @@ for i in range(n_steps):
     x_task = x_task_traj[:,0][np.newaxis].T
     u_task = u_task_traj[:,0][np.newaxis].T
 
+    # DEBUG
+    u_task = np.dot(R,u_lip) + np.dot(Q,x_lip) + np.dot(K , (x_task - np.dot(P,x_lip)))
+    u_task = np.clip(u_task,-500,500)
+
     # Record plottable values
     p_zmp_ref[i,:] = zmp_trajectory.value(t).flatten()
     p_zmp_lip[i,:] = u_lip.flatten()
@@ -226,6 +243,7 @@ for i in range(n_steps):
     # Simulate systems forward in time with Forward Euler
     x_lip = x_lip + dt*(np.dot(A_lip,x_lip) + np.dot(B_lip,u_lip))
     x_task = x_task + dt*(np.dot(A_task,x_task) + np.dot(B_task,u_task))
+
 
 ###############################################
 # Plot Results
